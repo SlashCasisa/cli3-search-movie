39952c866005e9d2329737ebc4f6c27b136c8042
梁彩霞 <836513735@qq.com>
2020-05-20 22:15:48 +0800

    打包

---
+++ dist/precache-manifest.4d71adef3009b91409a233a230a6fe37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9489bcc049f7a29684",
+    "url": "/css/app.8338d24e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "98cca0c5a8053c66680d55d9d4bf60a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1fea3c79b19c46f3dde",
+    "url": "/js/about.f0edd487.js"
+  },
+  {
+    "revision": "2d9489bcc049f7a29684",
+    "url": "/js/app.7b85e540.js"
+  },
+  {
+    "revision": "6b0f30ce7150edbeb00e",
+    "url": "/js/chunk-vendors.1198f602.js"
+  },
+  {
+    "revision": "ccf518da11bda859d0ee413f31b6f996",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
